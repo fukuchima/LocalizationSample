51e18f96ccc262beb3c4931ddd51ff5e131a207f
FMasayuki <FMasayuki@JP-TOOL-FKC-W10>
2016-12-05 18:38:41 +0900

    プロジェクト ファイルを追加します。

---
+++ LocalizationSample/LocalizationSample/MainView.cs
@@ -0,0 +1,41 @@
+﻿using LocalizationSample.Resources;
+using System;
+
+using Xamarin.Forms;
+
+namespace LocalizationSample
+{
+    public class MainView : ContentPage
+    {
+        int count = 0;
+        Button button = new Button();
+        Label label = new Label();
+
+        public MainView()
+        {
+            Title = "ローカライズサンプル";
+            button.Text = "タッチ";
+
+
+            button.Clicked += Button_Clicked;
+            label.HorizontalOptions = LayoutOptions.Center;
+
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.Center,
+            
+                Children = {
+                    label,
+                    button 
+                }
+            };
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count += 1;
+            label.Text = count.ToString() + "回タッチされました";
+        }
+    }
+}
